13e4c30d1106417347a30111853f71b2eb49c2e8
Matej Mohorcic <mohori.matej@gmail.com>
2023-03-02 08:17:51 +0100

    feat: New method in robotDriver.py
    Add moveJoint, pallet, getJoints. Add radius to moveL and moveJ.

--- robotDriver.py
+++ robotDriver.py
@@ -31,6 +31,7 @@ class Robot:
         self.robot.set_state(state=0)
         self.logger.info("Robot connected!")
         self.coordOffset = coordOffset
+        self.counter = 0
 
         if coordOffset != None:
             self.setWorldOffset(coordOffset)
@@ -46,6 +47,12 @@ class Robot:
 
     def drop(self):
         self.robot.close_lite6_gripper()
+
+    def gripperClose(self):
+        self.robot.close_lite6_gripper()
+
+    def gripperOpen(self):
+        self.robot.open_lite6_gripper()
 
     def home(self):
         self.robot.move_gohome(wait=True)
@@ -63,6 +70,9 @@ class Robot:
 
     def getPosition(self):
         return self.robot.get_position()[1]
+    
+    def getJoints(self):
+        return self.robot.get_servo_angle()[1]
 
     def set_position(self, x, y, z, speed, relative=False, wait=True):
         self.robot.set_position(
@@ -98,7 +108,7 @@ class Robot:
         self.setState(4)
 
     def moveL(self, x=None, y=None, z=None, roll=None, pitch=None, yaw=None, pose=None, speed=None, 
-              relative=False, wait=True, is_radian=False):
+              relative=False, wait=True, radius=0, is_radian=False):
         '''
         Move robot to position in linear motion
         
@@ -119,10 +129,10 @@ class Robot:
             x, y, z, roll, pitch, yaw = pose
 
         self.robot.set_position(x=x, y=y, z=z, roll=roll, pitch=pitch, yaw=yaw, speed=speed,
-                                relative=relative, wait=wait, mv_cmd=1)
+                                relative=relative, radius=radius, wait=wait, mv_cmd=1)
 
     def moveJ(self, x=None, y=None, z=None, roll=None, pitch=None, yaw=None, pose=None, speed=None,
-              wait=True, is_radian=False):
+              wait=True, radius=0, is_radian=False):
         '''
         Move robot to position in joint motion
         
@@ -155,6 +165,45 @@ class Robot:
                 yaw   = self.currentPosition[1][5]
 
             pose = [x, y, z, roll, pitch, yaw]
+        else:
+            pose = pose
 
         code, pose = self.robot.get_inverse_kinematics(pose, input_is_radian=is_radian, return_is_radian=False)
-        self.robot.set_servo_angle(angle=pose, speed=speed, wait=wait, is_radian=False)
+        self.robot.set_servo_angle(angle=pose, speed=speed, wait=wait, radius=radius, is_radian=False)
+
+    def moveJoint(self, joint1=None, joint2=None, joint3=None, joint4=None, joint5=None, joint6=None,
+                  speed=None, pose=None, wait=True, radius=0, is_radian=False):
+        '''
+        Move robot to position with joint angles
+        
+        :param joint1: joint 1 angle
+        :param joint2: joint 2 angle
+        :param joint3: joint 3 angle
+        :param joint4: joint 4 angle
+        :param joint5: joint 5 angle
+        :param joint6: joint 6 angle
+        :param pose: list of joint angles (alternative to separate parameters)
+        :param speed: speed of motion
+        :param wait: if True, wait for motion to complete
+        :param radius: radius of circular motion
+        :param is_radian: if True, angles are in radians
+        :return: None
+        '''
+        if pose == None:
+            pose = [joint1, joint2, joint3, joint4, joint5, joint6]
+
+        self.robot.set_servo_angle(angle=pose, speed=speed, wait=wait, radius=radius, is_radian=is_radian)
+
+    def pallet(self):
+        self.setWorldOffset([268.989075, 108.34774, -10.139146, -
+                            178.883382, 0.323492, 1.800291], is_radian=False)
+
+        indexX = self.counter % 4
+        indexY = self.counter // 4
+        width = 75.2957723090656 / 3
+        height = 74.3286143020664 / 3
+        self.moveJ(x=width * indexX, y=height * indexY, z=0, roll=0,
+                   pitch=0, yaw=0, speed=SPEED_FAST, wait=True)
+
+        self.counter += 1
+        self.setWorldOffset([0, 0, 0, 0, 0, 0], is_radian=False)
