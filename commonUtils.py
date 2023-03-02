46967a19090e5b43633870958784fc91dfc1cb3d
Vid Urh <vidurh48@gmail.com>
2023-03-02 18:53:07 +0100

    feat: :construction: Funkcionalen prototip z paletiziranjem
    Trenutno cikel deluje. Potrebno je še rekalibrirati točke in pretestirati edge-case

---
+++ commonUtils.py
@@ -0,0 +1,22 @@
+"""
+This script contains common utility functions used in the project.
+"""
+
+from loadPoints import Point
+import math
+
+
+def getDistance(point1: Point, point2: Point):
+    """
+        This method calculates the distance between two points.
+        It takes two points as arguments and returns the distance between them.
+    """
+    return ((point1.x - point2.x)**2 + (point1.y - point2.y)**2 + (point1.z - point2.z)**2)**.5
+
+
+def getAngle(point1: Point, point2: Point):
+    """
+        This method calculates the angle between two points.
+        It takes two points as arguments and returns the angle between them.
+    """
+    return math.acos((point1.x - point2.x) / getDistance(point1, point2))
--- main.py
+++ main.py
@@ -2,67 +2,79 @@ import robotDriver as rd
 from parameters import *
 import pandas as pd
 import time
+from loadPoints import *
 
-STEVILO_OHISIJ = 4
+STEVILO_OHISIJ = 7
 
-def readPoints():
-    # read csv file
-    with open(POINTS_PATH, 'r') as f:
-        points = pd.read_csv(f)
-
-    return points
 
 def main():
     robot = rd.Robot(ROBOT_IP)
 
-    points = readPoints()
+    pallet = rd.Pallet("4x4", points["pallet1.1"], points["pallet1.2"],
+                       points["pallet1.3"], points["pallet1.4"], 4, 4)
 
     # print point with name "start"
-    startPoint = points[points['name'] == 'start']
+    startPoint = points['start'].pose()
 
     # save start point to tuple
-    startPoint = tuple(startPoint.values[0][1:])
-    print(print(startPoint))
-    robot.moveJ(pose = tuple(points[points['name'] == 'start'].values[0][1:]), speed=SPEED_MIDDLE, wait=True)
-      
+    startPoint = tuple(startPoint)
+
+    robot.moveJ(pose=tuple(points["start"].pose()),
+                speed=SPEED_MIDDLE, wait=True)
 
     for i in range(STEVILO_OHISIJ):
-      ###############################
-      # start counting time
-      # move to start    
-      robot.gripperOpen()
-      start = time.time()
-      robot.moveJ(pose = tuple(points[points['name'] == 'start'].values[0][1:]), speed=SPEED_VERY_VERY_FAST, radius=250, wait=False)
-      time.sleep(0.2)
-      # move to ohisje
-      robot.moveJ(pose = tuple(points[points['name'] == 'nadOhisjem'].values[0][1:]), speed=SPEED_VERY_VERY_FAST, wait=True)
-      robot.moveL(pose = tuple(points[points['name'] == 'prijemOhisja'].values[0][1:]), speed=SPEED_FAST, wait=True)
-      robot.gripperClose()
-      time.sleep(0.1)
-      robot.moveL(pose = tuple(points[points['name'] == 'pritisneOhisje'].values[0][1:]), speed=SPEED_SLOW, wait=True)
-      robot.moveL(pose = tuple(points[points['name'] == 'nadOhisjem2'].values[0][1:]), speed=SPEED_MIDDLE, radius=20, wait=False)
+        ###############################
+        # start counting time
+        # move to start
+        start = time.time()
+        robot.gripperOpen()
 
-      # move to lepljenje
-      robot.moveJoint(pose = tuple(points[points['name'] == 'doLepila1.1'].values[0][1:7]), speed=SPEED_FAST, radius=40, wait=False)
-      #robot.moveJ(pose = tuple(points[points['name'] == 'doLepila1.1'].values[0][1:]), speed=SPEED_VERY_SLOW, radius=25, wait=False)
-      robot.moveJ(pose = tuple(points[points['name'] == 'doLepila2'].values[0][1:]), speed=SPEED_FAST, radius=60, wait=False)
-      robot.moveJoint(pose = tuple(points[points['name'] == 'podLepilom'].values[0][1:7]), speed=SPEED_VERY_FAST, wait=True)
+        robot.moveJ(pose=tuple(points["start"].pose(
+        )), speed=SPEED_VERY_VERY_FAST, radius=250, wait=False)
 
-      robot.moveL(pose = tuple(points[points['name'] == 'nanesiLepilo'].values[0][1:]), speed=10, wait=True)
-      time.sleep(0.1)
-      robot.moveL(pose = tuple(points[points['name'] == 'podLepilom2'].values[0][1:]), speed=SPEED_MIDDLE, wait=True)
+        time.sleep(0.2)
+        # move to ohisje
+        robot.moveJ(pose=tuple(points["nadOhisjem2"].pose()),
+                    speed=SPEED_VERY_VERY_FAST, wait=True)
 
-      # magnet
-      robot.moveJoint(pose = tuple(points[points['name'] == 'nadMagnetom'].values[0][1:]), speed=180, wait=True)
-      robot.moveL(pose = tuple(points[points['name'] == 'magnet'].values[0][1:]), speed=SPEED_SLOW, wait=True)
-      robot.gripperOpen()
-      time.sleep(0.1)
-      robot.moveL(pose = tuple(points[points['name'] == 'nadMagnetom2'].values[0][1:]), speed=SPEED_VERY_FAST, radius=5, wait=False)
-      time.sleep(0.1)
+        robot.moveL(pose=tuple(
+            points["prijemOhisja"].pose()), speed=SPEED_FAST, wait=True)
+        robot.gripperClose()
 
-      # calculate time
-      end = time.time()
-      print("Time: ", end - start)
+        time.sleep(0.1)
+        robot.moveL(pose=tuple(
+            points["pritisneOhisje"].pose()), speed=SPEED_SLOW, wait=True)
+
+        robot.moveL(pose=tuple(points["nadOhisjem2"].pose(
+        )), speed=SPEED_MIDDLE, radius=20, wait=False)
+
+        # move to lepljenje
+        robot.moveJoint(pose=tuple(
+            points["doLepila1.1"].pose()), speed=SPEED_FAST, radius=40, wait=False)
+        # robot.moveJ(pose = tuple(points[points['name'] == 'doLepila1.1'].values[0][1:]), speed=SPEED_VERY_SLOW, radius=25, wait=False)
+        robot.moveJ(pose=tuple(points["doLepila2"].pose()),
+                    speed=SPEED_FAST, radius=60, wait=False)
+        robot.moveJoint(pose=tuple(
+            points["podLepilom"].pose()), speed=SPEED_VERY_FAST, wait=True)
+
+        robot.moveL(pose=tuple(
+            points["nanesiLepilo"].pose()), speed=10, wait=True)
+        time.sleep(0.1)
+        robot.moveL(pose=tuple(
+            points["podLepilom2"].pose()), speed=SPEED_MIDDLE, wait=True)
+
+        robot.moveJ(pose=tuple(
+            points["nadMagneti"].pose()), speed=SPEED_VERY_VERY_FAST, radius=250, wait=False)
+
+        robot.pallet(pallet=pallet, robot=robot)
+
+        # calculate time
+        end = time.time()
+        print("Time: ", end - start)
+
+    robot.moveJ(pose=tuple(
+        points["start"].pose()), speed=SPEED_MIDDLE, wait=True)
+
 
 if __name__ == "__main__":
     main()
